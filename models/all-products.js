fd22119cbbfcdc2c54cbeabae5400908c57ffaa5
Kyle Nissley <knissley97@gmail.com>
2022-04-23 17:43:56 -0500

    Fix duplicate photo issue

--- models/all-products.js
+++ models/all-products.js
@@ -40,41 +40,86 @@ module.exports = {
         .catch(err => callback(err));
   },
 
-  getStyles: async (productId, callback) => {
-    const styleIdQuery = `
-    SELECT id
-    FROM Styles
-    WHERE product_id = ${productId}`;
+  getStyles: (productId, callback) => {
+    // const styleIdQuery = `
+    // SELECT id
+    // FROM Styles
+    // WHERE product_id = ${productId}`;
 
-    const styleIds = await products.query(styleIdQuery).then(res => res.rows);
+    // const styleIds = await products.query(styleIdQuery).then(res => res.rows);
 
-    const results = await Promise.all(styleIds.map(async (styleId) => {
-      const styleQuery = `
-      SELECT s.id AS style_id, s.name, s.sale_price, s.original_price, default_style AS "default?", json_agg(
+    // const results = await Promise.all(styleIds.map(async (styleId) => {
+    //   const styleQuery = `
+    //   SELECT
+    //     s.id AS style_id,
+    //     s.name,
+    //     s.sale_price,
+    //     s.original_price,
+    //     default_style AS "default?",
+    //     json_agg(
+    //       json_build_object(
+    //         'url', p.url,
+    //         'thumbnail_url', p.thumbnail_url
+    //       )
+    //     ) photos,
+    //     json_object_agg(
+    //       sk.id, json_build_object(
+    //         'size', sk.size,
+    //         'quantity', sk.quantity
+    //       )
+    //     ) skus
+    //   FROM Styles s
+    //   INNER JOIN Skus sk
+    //   ON sk.style_id = s.id
+    //   INNER JOIN Photos p
+    //   ON p.style_id = s.id
+    //   WHERE s.id = ${styleId.id}
+    //   GROUP BY s.id
+    //   `;
+
+    //   const style = await products.query(styleQuery).then(res => res.rows[0]);
+    //   return style;
+    // })).catch(err => callback(err.stack))
+
+    // callback(null, results);
+
+    const styleQuery = `
+    SELECT
+      s.id,
+      s.name,
+      s.sale_price,
+      s.original_price,
+      default_style AS "default?",
+      (SELECT json_agg(
         json_build_object(
           'url', p.url,
           'thumbnail_url', p.thumbnail_url
+          )
         )
-      ) photos, json_object_agg(
+        FROM Photos p
+        WHERE p.style_id = s.id) photos,
+      (SELECT json_object_agg(
         sk.id, json_build_object(
           'size', sk.size,
           'quantity', sk.quantity
+          )
         )
+        FROM Skus sk
+        WHERE sk.style_id = s.id
       ) skus
+    FROM Styles s
+    WHERE s.id IN (
+      SELECT s.id
       FROM Styles s
-      INNER JOIN Skus sk
-      ON sk.style_id = s.id
-      INNER JOIN Photos p
-      ON p.style_id = s.id
-      WHERE s.id = ${styleId.id}
-      GROUP BY s.id
-      `;
+      WHERE s.product_id = ${productId}
+    )
+    GROUP BY s.id
+    ORDER BY s.id
+    `;
 
-      const style = await products.query(styleQuery).then(res => res.rows[0]);
-      return style;
-    })).catch(err => callback(err.stack))
-
-    callback(null, results);
+    products.query(styleQuery)
+      .then(res => callback(null, res.rows))
+      .catch(err => callback(err.stack));
 
   },
 
